e9739b58b570054d3cba6efb43416aa2880ad632
Jeremy Archer <open-source@fatlotus.com>
2013-04-12 17:01:20 -0500

    Initial commit.

---
+++ blocky.py
@@ -0,0 +1,66 @@
+import byteplay
+
+def parallel_iterators(function):
+  source = byteplay.Code.from_code(main.func_code).code
+  
+  result_block = byteplay.Code.from_code(main.func_code)
+  
+  result = result_block.code
+  result[:] = [ ]
+  
+  skip_n_instructions = 0
+  
+  for index, (opcode, argument) in enumerate(source):
+    
+    if skip_n_instructions > 0:
+      skip_n_instructions -= 1
+      continue
+    
+    if opcode == byteplay.FOR_ITER:
+      
+      inside_this_loop = [ ]
+      
+      for op, arg in source[index + 1:]:
+        if op == argument: # loop destination
+          break
+        else:
+          inside_this_loop.append((op, arg))
+      
+      inside_this_loop.pop() # remove JUMP_ABSOLUTE
+      
+      def created_inner_function(__fixme):
+        pass
+      
+      new_code_block = byteplay.Code.from_code(created_inner_function.func_code)
+      
+      source_lines_break_stuff = [ (x, y) for (x, y) in inside_this_loop if x != byteplay.SetLineno ]
+      
+      new_code_block.code = (
+        [(byteplay.LOAD_FAST, '__fixme')] +
+        source_lines_break_stuff +
+        [(byteplay.LOAD_CONST, None),
+         (byteplay.RETURN_VALUE, None)]
+      )
+      
+      result.pop() # remove label
+      result.pop() # remove GET_ITER
+      
+      result.extend([
+        (byteplay.LOAD_ATTR, "iter"),
+        (byteplay.LOAD_CONST, new_code_block),
+        (byteplay.MAKE_FUNCTION, 0),
+        (byteplay.CALL_FUNCTION, 1),
+        (byteplay.POP_TOP, None)
+      ])
+      
+      skip_n_instructions = len(inside_this_loop) + 3
+      
+    elif opcode == byteplay.SETUP_LOOP:
+      
+      pass # FIXME
+      
+    else:
+      
+      result.append((opcode, argument))
+  
+  function.func_code = result_block.to_code()
---
+++ main.py
@@ -0,0 +1,27 @@
+import byteplay
+from blocky import parallel_iterators
+
+class Iterator(object):
+  def __init__(self, internal):
+    self.internal = internal
+  
+  def iter(self, callback):
+    for i in self.internal:
+      callback(i)
+
+class Entry(object):
+  @classmethod
+  def all(self):
+    return Iterator(xrange(0, 1000))
+
+@parallel_iterators
+def main():
+  print "Before loop"
+  
+  for entry in Entry.all():
+    print entry
+  
+  print "After loop"
+
+if __name__ == '__main__':
+  main()
